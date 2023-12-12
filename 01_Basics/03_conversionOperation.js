192d05357bfb280f397c991832b7aab99ea5158e
MeetMavani <99948701+MeetMavani@users.noreply.github.com>
2023-12-12 13:42:22 +0000

    learned about operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,9 +21,47 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 // "" => false
 // "meet" => true
 
-
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " meet"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2" + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100      // learn more about prefix and postfix from MDN docs
+++gameCounter;             // just search prefix and postfix js mdn
+console.log(gameCounter);
+
+// link to study more about conversions
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
