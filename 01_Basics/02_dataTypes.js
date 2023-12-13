61848cdab3d2b692ad5b3aa0860a6a37081bed74
MeetMavani <99948701+MeetMavani@users.noreply.github.com>
2023-12-13 11:08:04 +0000

    learned about hoisting in javascript

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -26,3 +26,17 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+// 
+const a = 1;
+console.log(a); // output: 1
+
+// but if 
+console.log(b);
+const b = 1; // output undefined 
+
+// this is called hoisting in JS 
+// what happens in JS is it puts the declaration of const b above the console.log(b)
+// only declaration thats why output is undefined
+// if you try to console.log(c) it will give error not defined as c is not declared yet.... 
