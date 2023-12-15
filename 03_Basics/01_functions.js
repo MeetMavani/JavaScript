9c0b476d22bc0dfd88fc27d13ae4b06c9bfe13dd
MeetMavani <99948701+MeetMavani@users.noreply.github.com>
2023-12-15 13:23:58 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -22,11 +22,54 @@ function addTwoNumbers(number1, number2){        // Parameters: number1 and numb
 
 
 function addTwoNumbers(number1, number2){        
-    const result = number1 + number2
-    return result                               // as per func definition rule log is unreachable
-    console.log("Meet");                        //  as it is after return statement      
+    // let result = number1 + number2
+    // return result                               // as per func definition rule log is unreachable
+    // console.log("Meet");                        //  as it is after return statement      
+    return number1 + number2
 }
 
 
 const result = addTwoNumbers(3,5)
-console.log("Result:", result);                     // undefined as function not returning anything
+// console.log("Result:", result);                     // undefined as function not returning anything
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("please enter a username");
+        return
+    }
+    return  `${username} just logged in`
+}
+
+// console.log(loginUserMessage("meet"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){           // rest operator    
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "meet",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log( `Username is ${anyobject.username} and price is ${anyobject["price"]}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 500, 600]));
