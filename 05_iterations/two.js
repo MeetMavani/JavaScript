6310a4410bb4a555b2dcc3a8662386750b551692
MeetMavani <99948701+MeetMavani@users.noreply.github.com>
2023-12-18 15:05:42 +0000

    while and do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+
+let i = 0
+while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// 
+
+// let score = 10
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 10);
